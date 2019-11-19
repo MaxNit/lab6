417a606dd7d9620513a2d7933f7f99ea3b50e1ea
MaxNit <46453376+MaxNit@users.noreply.github.com>
2019-11-19 21:10:49 +0300

    Update SAX.java

--- SAX.java
+++ SAX.java
@@ -1,40 +1,5 @@
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-public class SAX {
-    private static ArrayList<Product> products=new ArrayList<>();
-
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        SAXParser parser = factory.newSAXParser();
-
-        XMLHandler handler = new XMLHandler();
-        parser.parse(new File("4.xml"), handler);
-
-        for (Product product:products){
-            System.out.println(String.format("Type:%s\n"+"Brand:%s\n"+"Product:%s\n"+"Size:%s\n"+"Price:%s\n",
-                    product.getTypename(),product.getName(),product.getModel(),product.getSize(),product.getPrice()));
-        }
-
-    }
-    private static class XMLHandler extends DefaultHandler {
-        private String typename, name,model, size,price,lastElementName;
-        @Override
-        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            lastElementName = qName;
-        }
-
-        @Override
-        public void characters(char[] ch, int start, int length) throws SAXException {
-            String information = new String(ch, start, length);
 
             information = information.replace("\n", "").trim();
 
